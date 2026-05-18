cd2daec315a5f111288577cfbc232d0f404d8382
EdwinZhan <ed_zhan@outlook.com>
2026-05-18 00:52:33 -0400

    fix: add protoc build

--- crates/lumen-hub/build.rs
+++ crates/lumen-hub/build.rs
@@ -4,14 +4,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     let proto_file = "proto/ml_service.proto";
     let proto_dir = "proto";
     let out_dir = "src/daemon";
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
 
     println!("cargo:rerun-if-changed={proto_file}");
     println!("cargo:rerun-if-changed={proto_dir}");
 
+    let mut prost_config = tonic_prost_build::Config::new();
+    prost_config.protoc_executable(protoc);
+
     tonic_prost_build::configure()
         .out_dir(out_dir)
         .include_file("proto.rs")
-        .compile_protos(&[proto_file], &[proto_dir])?;
+        .compile_with_config(prost_config, &[proto_file], &[proto_dir])?;
 
     Ok(())
 }
